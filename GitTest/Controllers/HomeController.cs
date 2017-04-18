b6cf7a1f53a2411c39359969259d529a6ac1b3de
perceptivesolutions <contact@perinc.om>
2017-04-18 08:35:34 -0500

    Edited contact page title

--- GitTest/Controllers/HomeController.cs
+++ GitTest/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace GitTest.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page Test.";
 
             return View();
         }
